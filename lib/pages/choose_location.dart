490fdd3075ab0728cb526c4aab47c0b997bdd408
Emmanuel Egomson <femmor2u@gmail.com>
2020-08-12 09:04:28 -0500

    initial commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ChoodLocation extends StatefulWidget {
+  @override
+  _ChoodLocationState createState() => _ChoodLocationState();
+}
+
+class _ChoodLocationState extends State<ChoodLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Choose a location"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Text("Choose Location Screen"),
+    );
+  }
+}
+
